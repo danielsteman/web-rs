51ef8c0aabaf4bceb586bf3f115bbb5d1d631264
Daniel Steman <daniel.steman@bridgefund.nl>
2023-11-28 22:54:01 +0100

    remove comments

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,8 @@ where
     T: Template,
 {
     fn into_response(self) -> Response {
-        // Attempt to render the template with askama
         match self.0.render() {
-            // If we're able to successfully parse and aggregate the template, serve it
             Ok(html) => Html(html).into_response(),
-            // If we're not, return an error or some bit of fallback HTML
             Err(err) => (
                 StatusCode::INTERNAL_SERVER_ERROR,
                 format!("Failed to render template. Error: {}", err),
