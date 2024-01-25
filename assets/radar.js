b0061ac981c5b8aa887ed1863ab30ed8151fc50e
danielsteman <daniel-steman@live.nl>
2024-01-25 12:26:31 +0700

    add stuff to the radar

--- assets/radar.js
+++ assets/radar.js
@@ -1,16 +1,17 @@
 async function drawRadar() {
-  const data = await fetch("./radarEntries.json");
+  const data = await fetch("assets/radar.json");
+  const jsonData = await data.json();
   radar_visualization({
     svg_id: "radar",
     width: 1450,
     height: 1000,
     scale: 1.0,
     colors: {
-      background: "#fff",
-      grid: "#bbb",
+      background: "#f3f4f6",
+      grid: "#111827",
       inactive: "#ddd",
     },
-    title: "My Radar",
+    // title: "Personal Tech Radar",
     quadrants: [
       { name: "Web Development" },
       { name: "Database and Data Storage" },
@@ -18,191 +19,14 @@ async function drawRadar() {
       { name: "DevOps and Cloud Services" },
     ],
     rings: [
-      { name: "Adopt", color: "#5ba300" },
-      { name: "Trial", color: "#009eb0" },
-      { name: "Assess", color: "#c7ba00" },
-      { name: "Hold", color: "#e09b96" },
+      { name: "Adopt", color: "#a855f7" },
+      { name: "Trial", color: "#6366f1" },
+      { name: "Assess", color: "#ec4899" },
+      { name: "Hold", color: "#0ea5e9" },
     ],
     print_layout: true,
     links_in_new_tabs: true,
-    entries: [
-      {
-        label: "Python",
-        quadrant: 2, // 0,1,2,3 (counting clockwise, starting from bottom right)
-        ring: 0, // 0,1,2,3 (starting from inside)
-        moved: 0, // -1 = moved out (triangle pointing down)
-        //  0 = not moved (circle)
-        //  1 = moved in  (triangle pointing up)
-      },
-      {
-        label: "scikit-learn",
-        quadrant: 2,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "PySpark",
-        quadrant: 2,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "Pandas",
-        quadrant: 2,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "xgboost",
-        quadrant: 2,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "statsmodels",
-        quadrant: 2,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "Langchain",
-        quadrant: 2,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "SQL",
-        quadrant: 2,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "scala",
-        quadrant: 2,
-        ring: 2,
-        moved: 0,
-      },
-      {
-        label: "s3",
-        quadrant: 1,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "PostgreSQL",
-        quadrant: 1,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "SQLAlchemy",
-        quadrant: 1,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "Google Storage Bucket",
-        quadrant: 1,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "Firestore",
-        quadrant: 1,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "Alembic",
-        quadrant: 1,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "Redis",
-        quadrant: 1,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "Typescript",
-        quadrant: 0,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "Javascript",
-        quadrant: 0,
-        ring: 3,
-        moved: 0,
-      },
-      {
-        label: "Axum",
-        quadrant: 0,
-        ring: 1,
-        moved: 0,
-      },
-      {
-        label: "Rust",
-        quadrant: 0,
-        ring: 1,
-        moved: 0,
-      },
-      {
-        label: "Flask",
-        quadrant: 0,
-        ring: 3,
-        moved: 0,
-      },
-      {
-        label: "FastAPI",
-        quadrant: 0,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "Django",
-        quadrant: 0,
-        ring: 2,
-        moved: 0,
-      },
-      {
-        label: "React",
-        quadrant: 0,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "NextJS",
-        quadrant: 0,
-        ring: 0,
-        moved: 0,
-      },
-      {
-        label: "Vue",
-        quadrant: 0,
-        ring: 2,
-        moved: 0,
-      },
-      {
-        label: "HTMX",
-        quadrant: 0,
-        ring: 2,
-        moved: 0,
-      },
-      {
-        label: "TailwindCSS",
-        quadrant: 0,
-        ring: 2,
-        moved: 0,
-      },
-      {
-        label: "OpenAPI",
-        quadrant: 0,
-        ring: 0,
-        moved: 0,
-      },
-    ],
+    entries: jsonData.entries,
   });
 }
 
