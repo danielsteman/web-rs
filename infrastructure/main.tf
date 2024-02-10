f0d87021d318b05643c0bc4a8e4dc4fdbbb3c8a9
danielsteman <daniel-steman@live.nl>
2024-02-10 13:23:24 +0530

    remove redundant tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,16 +7,6 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "eu-central-1"
-}
-
-resource "aws_ecr_repository" "webrs" {
-  name                 = "webrs"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = false
-  }
 }
