c5284db7e66cc00da783d0be4589e7bd173e363e
Daniel Steman <daniel-steman@live.nl>
2026-01-17 20:20:42 +0100

    feat: better seo for blog articles

--- src/routes/blog.rs
+++ src/routes/blog.rs
@@ -1,3 +1,4 @@
+use time::macros::format_description;
 use time::Month;
 
 use axum::{
@@ -17,23 +18,63 @@ use sqlx::types::time::Date;
 struct BlogTemplate {
     id: i32,
     title: String,
+    summary: String,
     body: String,
     date: Date,
+    date_iso: String,
+    keywords: String,
+    tags_keywords: String,
+    has_tags: bool,
 }
 
 pub async fn blog(State(pool): State<PgPool>, Path(id): Path<i32>) -> impl IntoResponse {
     match Blog::get_blog(&pool, id).await {
-        Ok(blog) => HtmlTemplate(BlogTemplate {
-            id,
-            title: blog.title.clone(),
-            body: blog.body.clone(),
-            date: blog.date,
-        }),
-        Err(_) => HtmlTemplate(BlogTemplate {
-            id: 0,
-            title: "Not Found".to_string(),
-            body: "This blog hasn't been written yet...".to_string(),
-            date: Date::from_calendar_date(1995, Month::April, 13).unwrap(),
-        }),
+        Ok(blog) => {
+            // Format date as ISO 8601 for structured data
+            let date_format = format_description!("[year]-[month]-[day]");
+            let date_iso = blog.date.format(&date_format).unwrap_or_default();
+
+            // Create keywords string from tags
+            let keywords = if blog.tags.is_empty() {
+                "Daniel Steman, software engineering, tech blog".to_string()
+            } else {
+                format!(
+                    "{}, Daniel Steman, software engineering, tech blog",
+                    blog.tags.join(", ")
+                )
+            };
+
+            // Create tags keywords for structured data
+            let tags_keywords = blog.tags.join(", ");
+
+            HtmlTemplate(BlogTemplate {
+                id,
+                title: blog.title.clone(),
+                summary: blog.summary.clone(),
+                body: blog.body.clone(),
+                date: blog.date,
+                date_iso,
+                keywords,
+                tags_keywords,
+                has_tags: !blog.tags.is_empty(),
+            })
+        }
+        Err(_) => {
+            let error_date = Date::from_calendar_date(1995, Month::April, 13).unwrap();
+            let date_format = format_description!("[year]-[month]-[day]");
+            let date_iso = error_date.format(&date_format).unwrap_or_default();
+
+            HtmlTemplate(BlogTemplate {
+                id: 0,
+                title: "Not Found".to_string(),
+                summary: "Blog post not found".to_string(),
+                body: "This blog hasn't been written yet...".to_string(),
+                date: error_date,
+                date_iso,
+                keywords: "Daniel Steman, software engineering, tech blog".to_string(),
+                tags_keywords: String::new(),
+                has_tags: false,
+            })
+        }
     }
 }
